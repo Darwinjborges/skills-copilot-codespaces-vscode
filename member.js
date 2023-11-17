0038312d541f58513bceabd01b09ec53fd8a0a54
Darwin Borges <138389956+darwinjborges@users.noreply.github.com>
2023-11-17 03:21:46 +0000

    Copilot second commit

--- member.js
+++ member.js
@@ -1,15 +1,3 @@
-interface SkillsMember {
-    name: string;
-    age: number;
-    email: string;
-}
-
-function createMember(name: string, age: number, email: string): SkillsMember {
-    return { name, age, email };
-}
-    // properties and methods here
-}
-
-function createSkillsMember(): SkillsMember {
-    // implementation here
-}
+const member = () => {
+    // Your code here
+};
